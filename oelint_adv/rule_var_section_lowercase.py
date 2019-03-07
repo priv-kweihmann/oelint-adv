148e873581de911f0ef4fa0c0a2fbecf2793c847
Konrad Weihmann <kweihmann@outlook.com>
2019-03-07 14:14:50 +0100

    Added rule for lowercase SECTION
    Closes #1

---
+++ oelint_adv/rule_var_section_lowercase.py
@@ -0,0 +1,20 @@
+try:
+    from .cls_rule import Rule
+    from .cls_item import *
+except (SystemError, ImportError):
+    from cls_rule import Rule
+    from cls_item import *
+
+class VarSectionLowercase(Rule):
+    def __init__(self):
+        super().__init__(id = "oelint.vars.sectionlowercase", 
+                         severity="warning",
+                         message="'SECTION' should only lowercase characters")
+
+    def check(self, _file, stash):
+        res = []
+        items = stash.GetItemsFor(filename=_file, classifier=Variable.CLASSIFIER, attribute=Variable.ATTR_VAR, attvalue="SECTION")
+        for i in items:
+            if not i.VarValue.islower():
+                res += self.finding(i.Origin, i.InFileLine)
+        return res
