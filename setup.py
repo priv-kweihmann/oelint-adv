6a6f69e03a53abcda2016a73f0233b03c1eab67f
Konrad Weihmann <kweihmann@outlook.com>
2019-05-25 17:06:56 +0200

    Release 1.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="oelint_adv",
-    version="1.2.0",
+    version="1.3.0",
     author="Konrad Weihmann",
     author_email="kweihmann@outlook.com",
     description="Advanced bitbake-recipe linter",
