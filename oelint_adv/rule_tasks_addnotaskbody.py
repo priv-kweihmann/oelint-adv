9706afeb3263a4460df2b17b275974893f03ed53
Konrad Weihmann <kweihmann@outlook.com>
2019-05-25 14:51:32 +0200

    Warn on addtask without a task body
    Closes #16

---
+++ oelint_adv/rule_tasks_addnotaskbody.py
@@ -0,0 +1,26 @@
+try:
+    from .cls_rule import Rule
+    from .cls_item import *
+    from .const_func import FUNC_ORDER
+except (SystemError, ImportError):
+    from cls_rule import Rule
+    from cls_item import *
+    from const_func import FUNC_ORDER
+import os
+
+class TaskAddNoTaskBody(Rule):
+    def __init__(self):
+        super().__init__(id = "oelint.task.addnotaskbody", 
+                         severity="warning",
+                         message="The added task is not existing or has no body")
+
+    def check(self, _file, stash):
+        res = []
+        for item in stash.GetItemsFor(filename=_file, classifier=TaskAdd.CLASSIFIER):
+            if item.FuncName in FUNC_ORDER:
+                ## not for builtin types
+                continue
+            _ta = stash.GetItemsFor(filename=_file, classifier=Function.CLASSIFIER, attribute="FuncName", attributeValue=item.FuncName)
+            if not any(_ta):
+                res += self.finding(item.Origin, item.InFileLine)
+        return res
