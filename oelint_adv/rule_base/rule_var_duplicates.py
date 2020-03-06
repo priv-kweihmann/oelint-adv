88bc88ee1b27cef4317fc1daf2b3a2e78d04e359
Konrad Weihmann <kweihmann@outlook.com>
2020-03-06 17:30:19 +0100

    Fix duplicate var setting decection
    Mind that each var can have machine specifiers
    
    Closes #103

--- oelint_adv/rule_base/rule_var_duplicates.py
+++ oelint_adv/rule_base/rule_var_duplicates.py
@@ -14,12 +14,25 @@ class VarDuplicates(Rule):
         for c in ["DEPENDS", "RDEPENDS_${PN}"]:
             items = stash.GetItemsFor(filename=_file, classifier=Variable.CLASSIFIER,
                                       attribute=Variable.ATTR_VAR, attributeValue=c)
-            _items = []
+            _items = {}
             for i in items:
                 for x in [y for y in safe_linesplit(i.VarValueStripped) if y]:
-                    if x in _items:
-                        res += self.finding(i.Origin, i.InFileLine,
-                                            "Item '{}' was added multiple timesto {}".format(x, c))
+                    machine_mods = i.SubItems
+                    machine_mods_cleaned = "_".join(sorted([x for x in machine_mods if x not in ["append", "prepend", "remove"]]))
+                    if not machine_mods_cleaned in _items:
+                        _items[machine_mods_cleaned] = []
+                    _operations = i.AppendOperation()
+                    if x in _items[machine_mods_cleaned]:
+                        if not any([x in ["append", "prepend", " += ", " =+ "] for x in _operations]):
+                            _items[machine_mods_cleaned] = [x]
+                        else:
+                            res += self.finding(i.Origin, i.InFileLine,
+                                                "Item '{}' was added multiple times to {}".format(x, c))
                     else:
-                        _items.append(x)
+                        if any([x in ["append", "prepend", " += ", " =+ "] for x in _operations]):
+                            _items[machine_mods_cleaned].append(x)
+                        elif "remove" in _operations:
+                            _items[machine_mods_cleaned].remove(x)
+                        else:
+                            _items[machine_mods_cleaned] = [x]
         return res
