61b1b0a773f9e61c6ec61910c1668a73fd8f682f
Konrad Weihmann <kweihmann@outlook.com>
2020-03-06 17:39:38 +0100

    Release 1.15.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="oelint_adv",
-    version="1.15.3",
+    version="1.15.4",
     author="Konrad Weihmann",
     author_email="kweihmann@outlook.com",
     description="Advanced bitbake-recipe linter",
