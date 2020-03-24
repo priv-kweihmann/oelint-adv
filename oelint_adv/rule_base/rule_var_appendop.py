e084929e8bdafd2a7b375dbeb6af0fcb26b13cef
Konrad Weihmann <kweihmann@outlook.com>
2020-03-24 19:36:29 +0100

    Limit scope of oelint.vars.appendop
    - apply only to known weak assignments
    - change to error severity
    
    Closes #122

--- oelint_adv/rule_base/rule_var_appendop.py
+++ oelint_adv/rule_base/rule_var_appendop.py
@@ -5,16 +5,19 @@ from oelint_adv.cls_rule import Rule
 class VarAppendOperation(Rule):
     def __init__(self):
         super().__init__(id="oelint.vars.appendop",
-                         severity="warning",
+                         severity="error",
                          message="Use '_append' instead of ' += '")
 
     def check(self, _file, stash):
         res = []
         items = stash.GetItemsFor(
             filename=_file, classifier=Variable.CLASSIFIER)
-        for i in items:
-            if i.VarName == "inherit":
-                continue
-            if " += " in i.AppendOperation():
-                res += self.finding(i.Origin, i.InFileLine)
+        _names = set([x.VarName for x in items if x.VarName != "inherit"])
+        for name in _names:
+            _items = [x for x in items if x.VarName == name]
+            if len(_items) > 1:
+                _operations = set([x.VarOp for x in _items])
+                if " += " in _operations and any(x in _operations for x in [" ?= " , " ??= "]):
+                    for i in [x for x in _items if " += " in x.AppendOperation()]:
+                        res += self.finding(i.Origin, i.InFileLine)
         return res
