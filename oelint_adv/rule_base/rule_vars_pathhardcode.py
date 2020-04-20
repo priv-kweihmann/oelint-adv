5801202c14a10b598db9035c708d3eaf58ca2ef9
Konrad Weihmann <kweihmann@outlook.com>
2020-04-20 16:34:37 +0200

    Fix issue with pathhardcode check
    Closes #133
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- oelint_adv/rule_base/rule_vars_pathhardcode.py
+++ oelint_adv/rule_base/rule_vars_pathhardcode.py
@@ -2,7 +2,7 @@ from oelint_adv.cls_rule import Rule
 from oelint_adv.cls_item import Variable, Comment
 
 from collections import OrderedDict
-
+import re
 
 class VarsPathHardcode(Rule):
     def __init__(self):
@@ -40,20 +40,18 @@ class VarsPathHardcode(Rule):
                 continue
             _matches = []
             for k, v in _map.items():
-                for pre in ["'", "\"", " ", "${D}", "="]:
-                    for ext in [" ", "/", "\"", "*"]:
+                for pre in ["^", "'", "\"", " ", "\$\{D\}", "="]:
+                    for ext in [" ", "/", "\"", "\*", "$"]:
                         for line in i.get_items():
                             if line.strip().startswith("#"):
                                 continue
-                            _match = pre + k + ext in line
-                            _match |= pre + k in line
-                            _match |= k + ext in line
-                            _match |= k == line.strip()
+                            _match = re.search(pre + k + ext, line)
                             if _match and k not in _matches:
                                 _cleanapp = v.strip("$").strip("{").strip("}")
                                 res += self.finding(i.Origin, i.InFileLine,
                                                     "'{}' should be used instead of '{}'".format(v, k), appendix=_cleanapp)
                                 _parts = [x for x in k.split("/") if x]
-                                for x in range(0, len(_parts)):
-                                    _matches.append("/" + "/".join(_parts[0:x]))
+                                for x in range(1, len(_parts) + 1):
+                                    _k = "/" + "/".join(_parts[0:x])
+                                    _matches.append(_k)
         return res
