27916cafe6dab13afb25d09a0e5ada8213468cd5
Konrad Weihmann <kweihmann@outlook.com>
2020-05-02 12:23:53 +0200

    Fix tests in CI
    Remove hardcoded tmp dir
    Remove ignore of return value
    
    Closes #139
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
+export TMP="/tmp/"
+[ ! -d ${TMP} ] && mkdir ${TMP}
 export PATH=$(pwd)/bin:${PATH}
 for _test in $(pwd)/tests/*; do
-    /bin/sh ${_test} || true
+    /bin/sh ${_test} || exit 1
 done
