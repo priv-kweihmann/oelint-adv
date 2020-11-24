9c4ce2db03ca72e19b5882592064f0abf2c2e1e5
Konrad Weihmann <kweihmann@outlook.com>
2020-11-24 16:08:42 +0100

    Disable oelint.file.underscores for images
    all recipes that either inherit core-image or image or
    have IMAGE_INSTALL set are ignored to the rule
    
    Closes #191
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- oelint_adv/rule_base/rule_file_underscores.py
+++ oelint_adv/rule_base/rule_file_underscores.py
@@ -1,6 +1,8 @@
 import os
+import re
 
 from oelint_adv.cls_rule import Rule
+from oelint_parser.cls_item import Variable
 
 
 class FileNoSpaces(Rule):
@@ -14,6 +16,14 @@ class FileNoSpaces(Rule):
         res = []
         _basename, _ext = os.path.splitext(os.path.basename(_file))
         if _ext in [".bb"]:
+            _image_install = any(stash.GetItemsFor(filename=_file, classifier=Variable.CLASSIFIER,
+                                attribute=Variable.ATTR_VAR, attributeValue="IMAGE_INSTALL"))
+            _inherits = set()
+            for inh in stash.GetItemsFor(filename=_file, classifier=Variable.CLASSIFIER,
+                                  attribute=Variable.ATTR_VAR, attributeValue="inherit"):
+                _inherits.update([x.strip() for x in re.split(r"\s|,|\t|\x1b", inh.VarValue) if x])
+            if _image_install or any(x in _inherits for x in ["core-image", "image"]):
+                return res
             _sep = [x for x in _basename if x in ["_", "-"]]
             _us = [x for x in _sep if x == "_"]
             if len(_us) > 1:
