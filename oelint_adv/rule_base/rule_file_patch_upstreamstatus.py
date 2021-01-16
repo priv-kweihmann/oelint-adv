b27bae348a6a5f089289013aca19c7470ff39b1d
Konrad Weihmann <kweihmann@outlook.com>
2021-01-16 08:31:51 +0100

    More detailed check of Upstream-Status in patch
    https://www.openembedded.org/wiki/Commit_Patch_Message_Guidelines
    is now completly checked, include classifiers
    
    Closes #192
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- oelint_adv/rule_base/rule_file_patch_upstreamstatus.py
+++ oelint_adv/rule_base/rule_file_patch_upstreamstatus.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from oelint_parser.cls_item import Variable
 from oelint_adv.cls_rule import Rule
@@ -10,24 +11,47 @@ class FilePatchIsUpstreamStatus(Rule):
         super().__init__(id="oelint.file.upstreamstatus",
                          severity="info",
                          message="Patch '{FILE}' should contain an Upstream-Status entry")
+    
+    def _get_recipe(self, items, path):
+        # Find matching SRC_URI assignment
+        return [x for x in items if x.VarValue.find(os.path.basename(path)) != -1]
 
     def check(self, _file, stash):
         res = []
-        items = get_files(stash, _file, "*.patch")
+        patches = get_files(stash, _file, "*.patch")
         _items = stash.GetItemsFor(filename=_file, classifier=Variable.CLASSIFIER,
                                    attribute=Variable.ATTR_VAR, attributeValue="SRC_URI")
-        for i in items:
+
+        _valid_class = {
+            "Pending": r"Pending",
+            "Submitted": r"Submitted(\s+\[.*\])*",
+            "Accepted": r"Accepted",
+            "Denied": r"Denied",
+            "Backport": r"Backport",
+            "Inappropriate": r"Inappropriate\s+\[(oe-specific|OE specific|oe-core specific|not author|native|licensing|configuration|enable feature|disable feature|bugfix .*|embedded specific|no upstream|other)\]"
+        }
+        for i in patches:
             with open(i) as _input:
+                found = False
+                _recipe_match = self._get_recipe(_items, i)
+                if _recipe_match:
+                    _recipe_match = _recipe_match[0]
+                else:
+                    continue
                 try:
-                    content = _input.readlines()
-                    if not any([x for x in content if x.startswith("Upstream-Status: ")]):
-                        # Find matching SRC_URI assignment
-                        _assign = [x for x in _items if x.VarValue.find(
-                            os.path.basename(i)) != -1]
-                        if any(_assign):
-                            res += self.finding(_assign[0].Origin,
-                                                _assign[0].InFileLine,
-                                                self.Msg.replace("{FILE}", os.path.basename(i)))
+                    for m in re.finditer(r"^Upstream-Status:\s*(?P<class>.*)", _input.read(), re.MULTILINE):
+                        found = True
+                        if not any(re.match(v, m.group("class")) for k, v in _valid_class.items()):
+                            _msg = "Upstream-Status in '{FILE}' doesn't pick from valid classifiers {cls}".format(
+                                FILE=os.path.basename(i), cls=",".join(sorted(_valid_class.keys()))
+                            )
+                            res += self.finding(_recipe_match.Origin,
+                                        _recipe_match.InFileLine,
+                                        override_msg=_msg)
+                    if not found:
+                        res += self.finding(_recipe_match.Origin,
+                                            _recipe_match.InFileLine,
+                                            self.Msg.replace("{FILE}", os.path.basename(i)))
                 except UnicodeDecodeError:
                     pass
         return res
