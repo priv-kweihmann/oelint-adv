acbd6b9b052fe61c29786f717466ff66aa1fe170
Konrad Weihmann <kweihmann@outlook.com>
2021-02-25 11:58:48 +0100

    Warn about the usage of heredocs
    Closes #194
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- README.md
+++ README.md
@@ -117,6 +117,7 @@ Rules marked with **[S]** can have multiple sub-IDs
 * oelint.task.addnotaskbody - Task added by addtask cannot be found
 * oelint.task.customorder - order of custom tasks added via addtask
 * oelint.task.docstrings - Custom tasks should have docstrings
+* oelint.task.heredocs - Usage of heredocs should be avoided. Use files instead
 * oelint.task.multifragments - Multiple fragments of the same function in the same file should be merged
 * oelint.task.nocopy - No cp usage in do_install
 * oelint.task.nomkdir - No mkdir usage in do_install
---
+++ oelint_adv/rule_base/rule_tasks_heredocs.py
@@ -0,0 +1,21 @@
+import re
+from oelint_parser.cls_item import Function
+from oelint_adv.cls_rule import Rule
+
+
+class TaskInstallNoCp(Rule):
+    def __init__(self):
+        super().__init__(id="oelint.task.heredocs",
+                         severity="warning",
+                         message="Usage of heredocs should be avoided. Use files instead")
+
+    def check(self, _file, stash):
+        res = []
+        items = stash.GetItemsFor(filename=_file, classifier=Function.CLASSIFIER, 
+                                  attribute=Function.ATTR_FUNCNAME)
+        for i in items:
+            for index, line in enumerate(i.get_items()):
+                line = line.strip()
+                if re.match(r"^cat\s*>\s*.*<<\s*[A-Za-z0-9]+$", line) or re.match(r"^cat\s*<<\s*[A-Za-z0-9]+\s*>.*$", line):
+                    res += self.finding(i.Origin, i.InFileLine + index)
+        return res
