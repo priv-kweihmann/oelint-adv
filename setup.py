2fb3705327c46ac9e978af2180fa49f63b658813
Konrad Weihmann <kweihmann@outlook.com>
2021-04-23 14:04:46 +0200

    Release 2.3.1
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="oelint_adv",
-    version="2.2.5",
+    version="2.3.1",
     author="Konrad Weihmann",
     author_email="kweihmann@outlook.com",
     description="Advanced bitbake-recipe linter",
