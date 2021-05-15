19055dc6fd634787f8f5df2092be90e883d30140
Konrad Weihmann <kweihmann@outlook.com>
2021-05-15 21:27:11 +0200

    setup: prepare pytest setup
    including new requirements for development, some
    basic settings for pytest and upper version limits
    for the existing requirements
    
    Relates to #206
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     long_description=_long_description,
     long_description_content_type=_long_description_content_type,
     url="https://github.com/priv-kweihmann/oelint-adv",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=('tests',)),
     entry_points={
         "console_scripts": [
             "oelint-adv = oelint_adv.__main__:main",
