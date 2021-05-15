c54abf329ff3dbe333c44491290b4f10cb4ff336
Konrad Weihmann <kweihmann@outlook.com>
2021-05-15 21:43:27 +0200

    oelint-adv: remove python 3.5 support
    Closes #215
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setuptools.setup(
         "Natural Language :: English",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
