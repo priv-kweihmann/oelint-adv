8b63df2a06fc592fffff3fd3d9edfc125b973e4c
Konrad Weihmann <kweihmann@outlook.com>
2021-05-15 21:29:03 +0200

    vars.overrides: correctly handle weak assignments
    Closes #213
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- oelint_adv/rule_base/rule_vars_variable_override.py
+++ oelint_adv/rule_base/rule_vars_variable_override.py
@@ -17,14 +17,14 @@ class VarOverride(Rule):
         for v in __varnames:
             if v == "inherit" or not v:
                 # This will be done by another rule
-                continue
+                continue # pragma: no cover
             items = stash.GetItemsFor(filename=_file, classifier=Variable.CLASSIFIER, attribute=Variable.ATTR_VAR, attributeValue=v)
             items = sorted(items, key=lambda x: x.Line, reverse=False)
             for sub in [x.SubItem for x in items]:
                 # Get all entries but not the only that do immediate expansion,
                 # as these will be handled during parse time
                 # and apply to different rules
-                _items = [x for x in items if x.SubItem == sub and not x.IsAppend() and x.VarOp != " := " and not x.Flag]
+                _items = [x for x in items if x.SubItem == sub and not x.IsAppend() and x.VarOp not in [" := ", " ?= ", " ??= "] and not x.Flag]
                 if len(_items) > 1:
                     _files = list(set([os.path.basename(x.Origin) for x in _items]))
                     res += self.finding(_items[0].Origin, _items[0].InFileLine,
