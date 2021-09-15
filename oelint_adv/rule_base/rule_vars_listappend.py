priv-kweihmann/oelint-adv

commit 52e7ddbc97376d0f15231455e415bab4514ec1db

Fix mixup in error message
Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

oelint_adv/rule_base/rule_vars_listappend.py
@@ -19,7 +19,7 @@ class VarListAppend(Rule):
                 continue
             ops = i.AppendOperation()
             if not i.VarValue.startswith("\" ") and any(x in ops for x in ["append", " .= "]):
-                res += self.finding(i.Origin, i.InFileLine, override_msg="Prepend to list should end with a blank")
+                res += self.finding(i.Origin, i.InFileLine, override_msg="Append to list should end with a blank")
             if not i.VarValue.endswith(" \"") and any(x in ops for x in ["prepend", " =. "]):
-                res += self.finding(i.Origin, i.InFileLine, override_msg="Append to list should start with a blank")
+                res += self.finding(i.Origin, i.InFileLine, override_msg="Prepend to list should start with a blank")
         return res

commit e37d4fca08dee36ac6fa5c1d0479e58c89d4366d

listappend: Actually fix the mixup in the lint message
For appending, you need to start with a blank, for prepending you need
to end with a blank.

Fixes: 52e7ddbc9737 ("Fix mixup in error message")

oelint_adv/rule_base/rule_vars_listappend.py
@@ -19,7 +19,7 @@ class VarListAppend(Rule):
                 continue
             ops = i.AppendOperation()
             if not i.VarValue.startswith("\" ") and any(x in ops for x in ["append", " .= "]):
-                res += self.finding(i.Origin, i.InFileLine, override_msg="Append to list should end with a blank")
+                res += self.finding(i.Origin, i.InFileLine, override_msg="Append to list should start with a blank")
             if not i.VarValue.endswith(" \"") and any(x in ops for x in ["prepend", " =. "]):
-                res += self.finding(i.Origin, i.InFileLine, override_msg="Prepend to list should start with a blank")
+                res += self.finding(i.Origin, i.InFileLine, override_msg="Prepend to list should end with a blank")
         return res