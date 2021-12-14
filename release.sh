b6da245ae8d69fdd91dfb65be1017c9b82f7982f
Konrad Weihmann <kweihmann@outlook.com>
2021-12-14 11:55:46 +0100

    release.sh: run pre-commit hooks
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- release.sh
+++ release.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 ./build.sh
+pre-commit run --all-files
+[ $? -ne 0 ] && echo "Pre-commit hooks failed" && exit 1
 pytest
 [ $? -ne 0 ] && echo "Tests failed" && exit 1
 twine upload dist/*
