2af46edf026c11562c14dd793659a41e79774a8d
Konrad Weihmann <kweihmann@outlook.com>
2022-06-29 11:33:17 +0200

    Use oelint_parser for image detection
    and also omit checking packagegroups for oelint.file.underscores
    
    Closes #304
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- oelint_adv/rule_base/rule_file_underscores.py
+++ oelint_adv/rule_base/rule_file_underscores.py
@@ -1,8 +1,8 @@
 import os
 
 from oelint_adv.cls_rule import Rule
-from oelint_parser.cls_item import Variable
-from oelint_parser.rpl_regex import RegexRpl
+from oelint_parser.helper_files import is_image
+from oelint_parser.helper_files import is_packagegroup
 
 
 class FileNoSpaces(Rule):
@@ -16,15 +16,8 @@ class FileNoSpaces(Rule):
         res = []
         _basename, _ext = os.path.splitext(os.path.basename(_file))
         if _ext in ['.bb']:  # pragma: no cover
-            _image_install = any(stash.GetItemsFor(filename=_file, classifier=Variable.CLASSIFIER,
-                                                   attribute=Variable.ATTR_VAR, attributeValue='IMAGE_INSTALL'))
-            _inherits = set()
-            for inh in stash.GetItemsFor(filename=_file, classifier=Variable.CLASSIFIER,
-                                         attribute=Variable.ATTR_VAR, attributeValue='inherit'):
-                _inherits.update([x.strip() for x in RegexRpl.split(
-                    r'\s|,|\t|\x1b', inh.VarValue) if x])
-            if _image_install or any(x in _inherits for x in ['core-image', 'image']):
-                return res
+            if is_packagegroup(stash, _file) or is_image(stash, _file):
+                return []
             _sep = [x for x in _basename if x in ['_', '-']]
             _us = [x for x in _sep if x == '_']
             if len(_us) > 1:
--- tests/test_class_oelint_file_underscores.py
+++ tests/test_class_oelint_file_underscores.py
@@ -50,7 +50,40 @@ class TestClassOelintFileUnderscores(TestBaseClass):
                                      'oelint-adv_git.bb':
                                      'VAR = "a"',
                                  },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'inherit packagegroup',
+                                 },
                              ],
                              )
     def test_good(self, input, id, occurrence):
         self.check_for_id(self._create_args(input), id, occurrence)
+
+    @pytest.mark.parametrize('id', ['oelint.file.underscores'])
+    @pytest.mark.parametrize('occurrence', [0])
+    @pytest.mark.parametrize('input',
+                             [
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'inherit my-image',
+                                 },
+                             ],
+                             )
+    def test_good_image_mod(self, input, id, occurrence):
+        __cnt = '''
+        {
+            "images": {
+                "known-classes": [
+                    "my-image"
+                ]
+            },
+            "functions": {
+                "order": ["do_foo"]
+            }
+        }
+        '''
+        _extra_opts = [
+            '--constantmods=+{mod}'.format(mod=self._create_tempfile('constmod', __cnt))]
+        _args = self._create_args(input, extraopts=_extra_opts)
+
+        self.check_for_id(_args, id, occurrence)
