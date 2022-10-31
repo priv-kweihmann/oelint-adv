83ca01d63129d58cbd72fe4ec0749439d86c3880
Konrad Weihmann <kweihmann@outlook.com>
2022-10-31 14:39:05 +0000

    Add python 3.11 support
    and the missing linter stage in the CI pipeline.
    Also split up PRs and pushes in the pipeline
    
    Closes #320
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Quality Assurance',
     ],
