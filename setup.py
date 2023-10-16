6fc350f1a1c5432194e33b0513d2747f23dbdbf6
offa <bm-dev@yandex.com>
2023-10-16 20:49:15 +0200

    Add Python 3.12 CI build

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Quality Assurance',
     ],
