a326dc910707827dcb027e08d7d29f976d1fb3ed
Konrad Weihmann <kweihmann@outlook.com>
2024-02-08 06:46:56 +0100

    inheritdevtool: recommend inherit_defer
    for native and nativesdk class, if the configured release
    supports that statement
    
    Closes #500
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

---
+++ oelint_adv/rule_base/rule_vars_inherit_devtool.py
@@ -0,0 +1,31 @@
+from typing import List, Tuple
+
+from oelint_parser.cls_item import Inherit
+from oelint_parser.cls_stash import Stash
+
+from oelint_adv.cls_rule import Rule
+
+
+class VarInheritDevtool(Rule):
+    def __init__(self) -> None:
+        super().__init__(id='oelint.var.inheritdevtool',
+                         severity='warning',
+                         message='It is better to use inherit_defer for {statement}, to enable proper devtool integration',
+                         appendix=['native', 'nativesdk'],
+                         valid_from_release='scarthgap')
+
+    def check(self, _file: str, stash: Stash) -> List[Tuple[str, int, str]]:
+        res = []
+        items: List[Inherit] = stash.GetItemsFor(filename=_file, classifier=Inherit.CLASSIFIER)
+        for i in items:
+            if 'native' in i.get_items() and i.Statement != 'inherit_defer':
+                res += self.finding(i.Origin, i.InFileLine,
+                                    override_msg=self.Msg.format(statement='native'),
+                                    appendix='native',
+                                    )
+            if 'nativesdk' in i.get_items() and i.Statement != 'inherit_defer':
+                res += self.finding(i.Origin, i.InFileLine,
+                                    override_msg=self.Msg.format(statement='nativesdk'),
+                                    appendix='nativesdk',
+                                    )
+        return res
