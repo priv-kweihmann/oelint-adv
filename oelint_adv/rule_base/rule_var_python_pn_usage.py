f99fb97bafd6685a3f3560608bdeb18697d6a231
Konrad Weihmann <kweihmann@outlook.com>
2024-02-23 07:41:31 +0100

    oelint.vars.pythonpnusage: add rule
    to notify about discontinuing of PYTHON_PN by upstream
    from scarthgap on.
    Rule only works on bb and bbappends, leaving out class code
    
    Closes #514
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

---
+++ oelint_adv/rule_base/rule_var_python_pn_usage.py
@@ -0,0 +1,52 @@
+from typing import List, Tuple
+
+from oelint_parser.cls_item import Function, Item, PythonBlock, Variable
+from oelint_parser.cls_stash import Stash
+
+from oelint_adv.cls_rule import Rule
+
+
+class VarPythonPnUsage(Rule):
+    def __init__(self) -> None:
+        super().__init__(id='oelint.vars.pythonpnusage',
+                         severity='info',
+                         message='python3 should be used instead of ${PYTHON_PN}',
+                         valid_from_release='scarthgap')
+
+    def __getMatches(self, _file: str, stash: Stash) -> List[Tuple[Item, List[str]]]:
+        res = []
+        items: List[Item] = stash.GetItemsFor(filename=_file,
+                                              classifier=[
+                                                  Variable.CLASSIFIER,
+                                                  Function.CLASSIFIER,
+                                                  PythonBlock.CLASSIFIER,
+                                              ])
+        for i in items:
+            if i.Origin.endswith('.bbclass'):
+                continue
+            if isinstance(i, PythonBlock):
+                needles = ['${PYTHON_PN}', 'd.getVar("PYTHON_PN")', 'd.getVar(\'PYTHON_PN\')']
+            elif isinstance(i, (Function, Variable)):
+                needles = ['${PYTHON_PN}']
+            else:
+                needles = []  # pragma: no cover
+            if any(x in i.Raw for x in needles):
+                res.append((i, needles))
+        return res
+
+    def check(self, _file: str, stash: Stash) -> List[Tuple[str, int, str]]:
+        res = []
+        for i in self.__getMatches(_file, stash):
+            item, _ = i
+            res += self.finding(item.Origin, item.InFileLine)
+        return res
+
+    def fix(self, _file: str, stash: Stash) -> List[str]:
+        res = []
+        for i in self.__getMatches(_file, stash):
+            item, needles = i
+            for needle in needles:
+                item.RealRaw = item.RealRaw.replace(needle, 'python3')
+                item.Raw = item.Raw.replace(needle, 'python3')
+            res.append(_file)
+        return res
---
+++ tests/test_class_oelint_vars_pythonpnusage.py
@@ -0,0 +1,219 @@
+import pytest  # noqa: I900
+
+from .base import TestBaseClass
+
+
+class TestClassOelintVarsPythonPnUsage(TestBaseClass):
+
+    @pytest.mark.parametrize('id_', ['oelint.vars.pythonpnusage'])
+    @pytest.mark.parametrize('occurrence', [1])
+    @pytest.mark.parametrize('input_',
+                             [
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'A = "${PYTHON_PN}"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'A = "${PYTHON_PN}/${PYTHON_PN}"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'A_${PYTHON_PN} = "1"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     do_something() {
+                                        echo ${PYTHON_PN} > test.txt
+                                     }
+                                     ''',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     def do_something():
+                                        d.getVar("PYTHON_PN")
+                                     ''',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     def do_something():
+                                        d.getVar(\'PYTHON_PN\')
+                                     ''',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     def do_something():
+                                        d.expandVar("${PYTHON_PN}/A")
+                                     ''',
+                                 },
+                             ],
+                             )
+    def test_bad(self, input_, id_, occurrence):
+        self.check_for_id(self._create_args(input_, ['--release=scarthgap']), id_, occurrence)
+
+    @pytest.mark.parametrize('id_', ['oelint.vars.pythonpnusage'])
+    @pytest.mark.parametrize('occurrence', [0])
+    @pytest.mark.parametrize('input_',
+                             [
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'A = "${PYTHON_PN}"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'A = "${PYTHON_PN}/${PYTHON_PN}"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'A_${PYTHON_PN} = "1"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     do_something() {
+                                        echo ${PYTHON_PN} > test.txt
+                                     }
+                                     ''',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     def do_something():
+                                        d.getVar("PYTHON_PN")
+                                     ''',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     def do_something():
+                                        d.getVar(\'PYTHON_PN\')
+                                     ''',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     def do_something():
+                                        d.expandVar("${PYTHON_PN}/A")
+                                     ''',
+                                 },
+                             ],
+                             )
+    def test_bad_old(self, input_, id_, occurrence):
+        self.check_for_id(self._create_args(input_), id_, occurrence)
+
+    @pytest.mark.parametrize('id_', ['oelint.vars.pythonpnusage'])
+    @pytest.mark.parametrize('input_',
+                             [
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'A = "${PYTHON_PN}"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'A = "${PYTHON_PN}/${PYTHON_PN}"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'A_${PYTHON_PN} = "1"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     do_something() {
+                                        echo ${PYTHON_PN} > test.txt
+                                     }
+                                     ''',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     def do_something():
+                                        d.getVar("PYTHON_PN")
+                                     ''',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     def do_something():
+                                        d.getVar(\'PYTHON_PN\')
+                                     ''',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     def do_something():
+                                        d.expandVar("${PYTHON_PN}/A")
+                                     ''',
+                                 },
+                             ],
+                             )
+    def test_fix(self, input_, id_):
+        self.fix_and_check(self._create_args_fix(input_, ['--release=scarthgap']), id_)
+
+    @pytest.mark.parametrize('id_', ['oelint.vars.pythonpnusage'])
+    @pytest.mark.parametrize('occurrence', [0])
+    @pytest.mark.parametrize('input_',
+                             [
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'S = "${WORDKIR}/${BP}"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'SRCREV_PYTHON_PN = "01234567890abcdef"',
+                                 },
+                                 {
+                                     'classes/test.bbclass':
+                                     'A = "${PYTHON_PN}"',
+                                     'oelint_adv_test.bb':
+                                     'inherit test',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'A = "python3"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'A = "python3/python3"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'A_python3 = "1"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     do_something() {
+                                        echo python3 > test.txt
+                                     }
+                                     ''',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     def do_something():
+                                        d.getVar("somethingelse")
+                                     ''',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     def do_something():
+                                        d.getVar(\'somethingelse\')
+                                     ''',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     def do_something():
+                                        d.expandVar("python3/A")
+                                     ''',
+                                 },
+                             ],
+                             )
+    def test_good(self, input_, id_, occurrence):
+        self.check_for_id(self._create_args(input_, ['--release=scarthgap']), id_, occurrence)
