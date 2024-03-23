290202ff65b5320b4f2f90038601b96414e3dc4f
Konrad Weihmann <kweihmann@outlook.com>
2024-03-23 11:23:36 +0100

    mandatory-vars: allow fine tuning
    by using the new custom constant feature of the parser
    
    Closes #540
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- docs/wiki/oelint.var.mandatoryvar.md
+++ docs/wiki/oelint.var.mandatoryvar.md
@@ -32,3 +32,39 @@ HOMEPAGE = "https://my-homepage.com"
 LICENSE = "MIT"
 SRC_URI = "https://my-homepage.com/foo_${PV}.zip"
 ```
+
+### Fine tuning
+
+To fine tuned the behavior on the rule, you can apply the following [constants](https://github.com/priv-kweihmann/oelint-adv#adding-additional-constants)
+
+- `oelint-mandatoryvar/pkggroup-excludes` to skip mandatory variables in a packagegroup
+- `oelint-mandatoryvar/image-excludes` to skip mandatory variables in images
+- `oelint-mandatoryvar/srcuri-exclude-classes` to skip `SRC_URI`, because of a class inherited that defines `SRC_URI` already
+
+e.g. if your custom class ``foo.bbclass`` contains
+
+```
+SRC_URI ?= "http://foo.com/{BPN}.zip"
+```
+
+but resides in a different layer than the current, you can run the linter with
+
+```console
+oelint-adv --constantmods=+.my-custom-src-uri-classes.json <other options> <file>...
+```
+
+with `.my-custom-src-uri-classes.json` being
+
+```json
+{
+    "oelint-mandatoryvar": {
+        "srcuri-exclude-classes": [
+            "foo"
+        ]
+    }
+}
+```
+
+to let the linter know that this is fine in the given context.
+
+See also the [Configuration file](https://github.com/priv-kweihmann/oelint-adv#configuration-file) option to automatically apply that to the layer.
--- oelint_adv/rule_base/rule_vars_mandatory_exists.py
+++ oelint_adv/rule_base/rule_vars_mandatory_exists.py
@@ -14,23 +14,44 @@ class VarMandatoryExists(Rule):
                          message='<FOO>',
                          onappend=False,
                          appendix=CONSTANTS.VariablesMandatory)
+        CONSTANTS.AddConstants(
+            {
+                'oelint-mandatoryvar':
+                {
+                    'image-excludes': [
+                        'CVE_PRODUCT',
+                        'HOMEPAGE',
+                        'SRC_URI',
+                    ],
+                },
+            },
+        )
 
-    IMAGE_EXCLUDES = [
-        'CVE_PRODUCT',
-        'HOMEPAGE',
-        'SRC_URI',
-    ]
+        CONSTANTS.AddConstants(
+            {
+                'oelint-mandatoryvar':
+                {
+                    'pkggroup-excludes': [
+                        'CVE_PRODUCT',
+                        'HOMEPAGE',
+                        'LICENSE',
+                        'SRC_URI',
+                    ],
+                },
+            },
+        )
 
-    PACKAGEGRP_EXCLUDES = [
-        'CVE_PRODUCT',
-        'HOMEPAGE',
-        'LICENSE',
-        'SRC_URI',
-    ]
-
-    SRC_URI_EXLCUDES = [
-        'SRC_URI',
-    ]
+        CONSTANTS.AddConstants(
+            {
+                'oelint-mandatoryvar':
+                {
+                    'srcuri-exclude-classes': [
+                        'pypi',
+                        'gnomebase',
+                    ],
+                },
+            },
+        )
 
     def check(self, _file: str, stash: Stash) -> List[Tuple[str, int, str]]:
         res = []
@@ -42,14 +63,15 @@ class VarMandatoryExists(Rule):
                                                   attributeValue=CONSTANTS.VariablesMandatory)
         inherits: List[Inherit] = stash.GetItemsFor(filename=_file,
                                                     classifier=Inherit.CLASSIFIER)
-        # pypi and gnomebase base set SRC_URI on their own, do not warn here
-        src_setting_inherits = any(True for x in inherits if 'pypi' in x.get_items() or 'gnomebase' in x.get_items())
+        # some classes set SRC_URI on their own, do not warn here
+        src_setting_inherits = any(True for x in inherits if any(y in x.get_items()
+                                   for y in CONSTANTS.GetByPath('oelint-mandatoryvar/srcuri-exclude-classes')))
         for var_ in CONSTANTS.VariablesMandatory:
-            if _is_pkg_group and var_ in VarMandatoryExists.PACKAGEGRP_EXCLUDES:
+            if _is_pkg_group and var_ in CONSTANTS.GetByPath('oelint-mandatoryvar/pkggroup-excludes'):
                 continue
-            if _is_image and var_ in VarMandatoryExists.IMAGE_EXCLUDES:
+            if _is_image and var_ in CONSTANTS.GetByPath('oelint-mandatoryvar/image-excludes'):
                 continue
-            if src_setting_inherits and var_ in VarMandatoryExists.SRC_URI_EXLCUDES:
+            if src_setting_inherits and var_ in ['SRC_URI']:
                 continue
             filtered = stash.Reduce(items, attribute=Variable.ATTR_VAR, attributeValue=var_)
             if not any(filtered):
