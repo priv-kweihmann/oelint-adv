02672a2001e52ef3e8279de82d9bc8e89a827d2e
Konrad Weihmann <kweihmann@outlook.com>
2024-03-30 11:45:32 +0100

    rules: fix description rules
    by expanding the value properly before doing any check
    
    Closes #553
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- oelint_adv/rule_base/rule_var_descriptiontooshort.py
+++ oelint_adv/rule_base/rule_var_descriptiontooshort.py
@@ -14,12 +14,11 @@ class VarDescSameTooBrief(Rule):
 
     def check(self, _file: str, stash: Stash) -> List[Tuple[str, int, str]]:
         res = []
+        summary = ''.join(stash.ExpandVar(_file, attribute=Variable.ATTR_VAR, attributeValue='SUMMARY').get('SUMMARY', ['']))
+        desc = ''.join(stash.ExpandVar(_file, attribute=Variable.ATTR_VAR,
+                       attributeValue='DESCRIPTION').get('DESCRIPTION', ['']))
         items: List[Variable] = stash.GetItemsFor(filename=_file, classifier=Variable.CLASSIFIER,
                                                   attribute=Variable.ATTR_VAR, attributeValue='DESCRIPTION')
-        items_sum: List[Variable] = stash.GetItemsFor(filename=_file, classifier=Variable.CLASSIFIER,
-                                                      attribute=Variable.ATTR_VAR, attributeValue='SUMMARY')
-        for i in items:
-            _same = [x for x in items_sum if len(x.VarValueStripped) > len(i.VarValueStripped)]
-            if any(_same):
-                res += self.finding(i.Origin, i.InFileLine)
+        if len(desc.strip()) < len(summary.strip()) and items:
+            res += self.finding(items[0].Origin, items[0].InFileLine)
         return res
