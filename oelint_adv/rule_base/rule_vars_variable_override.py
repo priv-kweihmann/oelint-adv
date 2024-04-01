0aedbdebe6a878dca5066e4dfd1dabf2ad58f5d2
Vyacheslav Yurkov <uvv.mail@gmail.com>
2024-04-01 20:40:20 +0200

    rules: Fix override false positive
    Fix #557
    
    Signed-off-by: Vyacheslav Yurkov <uvv.mail@gmail.com>

--- oelint_adv/rule_base/rule_vars_variable_override.py
+++ oelint_adv/rule_base/rule_vars_variable_override.py
@@ -17,7 +17,7 @@ class VarOverride(Rule):
         res = []
         _all: List[Variable] = list(x for x in stash.GetItemsFor(filename=_file, classifier=Variable.CLASSIFIER))  # noqa: C400
         for v in {x.VarName for x in _all}:
-            items = [x for x in _all if x.VarName == v]
+            items = [x for x in _all if x.VarName == v and x.IsImmediateModify()]
             items = sorted(items, key=lambda x: x.Line, reverse=False)
             for sub in [x.SubItem for x in items]:
                 # Get all entries but not the only that do immediate expansion,
