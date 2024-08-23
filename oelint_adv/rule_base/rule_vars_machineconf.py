dbbe6c43726d0cf9b47090e3536fce7d5d415fa1
Konrad Weihmann <kweihmann@outlook.com>
2024-08-23 06:09:05 +0000

    oelint.vars.machineconf: add new rule
    to check for discouraged variables in a machine conf
    
    Relates to #607
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

---
+++ oelint_adv/rule_base/rule_vars_machineconf.py
@@ -0,0 +1,39 @@
+import fnmatch
+from typing import List, Tuple
+
+from oelint_parser.cls_item import Variable
+from oelint_parser.cls_stash import Stash
+
+from oelint_adv.cls_rule import Rule
+
+
+class VarsMachineConf(Rule):
+    def __init__(self) -> None:
+        self.needles = [
+            'DISTROOVERRIDES',
+            'DISTRO_EXTRA_RDEPENDS',
+            'DISTRO_EXTRA_RRECOMMENDS',
+            'DISTRO_FEATURES',
+            'DISTRO_FEATURES_BACKFILL',
+            'DISTRO_FEATURES_BACKFILL_CONSIDERED',
+            'DISTRO_FEATURES_DEFAULT',
+            'IMAGE_INSTALL',
+        ]
+
+        super().__init__(id='oelint.vars.machineconf',
+                         severity='warn',
+                         message='{var} should not be set as part of a machine configuration',
+                         appendix=self.needles)
+
+    def check(self, _file: str, stash: Stash) -> List[Tuple[str, int, str]]:
+        res = []
+        items: List[Variable] = stash.GetItemsFor(filename=_file, classifier=Variable.CLASSIFIER,
+                                                  attribute=Variable.ATTR_VAR)
+
+        for i in items:
+            if not fnmatch.fnmatch(i.Origin, '*/machine/*.conf'):
+                continue
+            if i.VarName in self.needles:
+                res += self.finding(_file, i.Line,
+                                    self.Msg.format(var=i.VarName), appendix=i.VarName)
+        return res
