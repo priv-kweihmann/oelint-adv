9c239d636f761e3d0e8e3d8e7b33fe81a127438d
Konrad Weihmann <kweihmann@outlook.com>
2024-10-27 06:36:14 +0000

    oelint.task.nopythonprefix: tweak message
    and explicitly mention that the code in the task
    needs to be syntactically correct python code to
    pass the check
    
    Relates to #639
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- oelint_adv/rule_base/rule_tasks_nopython_prefix.py
+++ oelint_adv/rule_base/rule_tasks_nopython_prefix.py
@@ -11,7 +11,7 @@ class TaskNoPythonPrefix(Rule):
     def __init__(self) -> None:
         super().__init__(id='oelint.task.nopythonprefix',
                          severity='warning',
-                         message='Tasks containing shell code, should not be prefixed with python in function header')
+                         message='Tasks that don\'t contain valid python code (e.g. shell code), should not be prefixed with python in function header')
 
     def check(self, _file: str, stash: Stash) -> List[Tuple[str, int, str]]:
         res = []
