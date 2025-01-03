224808e934a689b19e63172006d5adeec8d9fc16
Konrad Weihmann <kweihmann@outlook.com>
2025-01-03 09:47:01 +0100

    core: fix inline suppression
    - allow '-' in id
    - rewrite filtering to allow multiple items to be mapped
      better to the workflow
    
    Closes #677
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- tests/test_inlinesuppressions.py
+++ tests/test_inlinesuppressions.py
@@ -56,7 +56,7 @@ class TestClassInlineSuppressions(TestBaseClass):
     def test_inlinesuppressions_single_notmatching(self, input_):
         self.check_for_id(self._create_args(input_),
                           'oelint.vars.insaneskip', 1)
-        
+
     @pytest.mark.parametrize('input_',
                              [
                                  {
@@ -80,7 +80,7 @@ class TestClassInlineSuppressions(TestBaseClass):
                                     # nooelint: oelint.var.suggestedvar.BUGTRACKER,oelint.var.mandatoryvar.SUMMARY,oelint.var.bbclassextend
                                     INSANE_SKIP_${PN} = "foo"
                                     ''',
-                                 }
+                                 },
                              ],
                              )
     def test_inlinesuppressions_line_1(self, input_):
@@ -90,6 +90,23 @@ class TestClassInlineSuppressions(TestBaseClass):
                           'oelint.var.mandatoryvar.SUMMARY', 0)
         self.check_for_id(self._create_args(input_),
                           'oelint.var.bbclassextend', 0)
+
+    @pytest.mark.parametrize('input_',
+                             [
+                                 {
+                                     'oelint adv-test.bb':
+                                     '''
+                                     # nooelint: oelint.var.badimagefeature.allow-empty-password,oelint.var.badimagefeature.allow-root-login,oelint.var.badimagefeature.empty-root-password
+                                     IMAGE_FEATURES:append = " \
+                                        allow-empty-password allow-root-login empty-root-password \
+                                     "
+                                     ''',
+                                 },
+                             ],
+                             )
+    def test_inlinesuppressions_line_2(self, input_):
+        self.check_for_id(self._create_args(input_),
+                          'oelint.var.badimagefeature', 0)
 
     @pytest.mark.parametrize('input_',
                              [
@@ -128,3 +145,20 @@ class TestClassInlineSuppressions(TestBaseClass):
                           'oelint.vars.doublemodify', 1)
         self.check_for_id(self._create_args(input_),
                           'oelint.vars.overrideappend', 1)
+
+    @pytest.mark.parametrize('input_',
+                             [
+                                 {
+                                     'oelint adv-test.bb':
+                                     '''
+                                     # nooelint: oelint.var.badimagefeature.allow-empty-password,,oelint.var.badimagefeature.allow-root-login,oelint.var.badimagefeature.empty-root-password
+                                     IMAGE_FEATURES:append = " \
+                                        allow-empty-password allow-root-login empty-root-password \
+                                     "
+                                     ''',
+                                 },
+                             ],
+                             )
+    def test_inlinesuppressions_remove_empty(self, input_):
+        self.check_for_id(self._create_args(input_),
+                          'oelint.var.badimagefeature', 0)
