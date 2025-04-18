2334c1062de315162cbaa7b023dd9751c840f767
Konrad Weihmann <kweihmann@outlook.com>
2025-04-18 08:27:52 +0000

    newline.consecutive: remove fix method
    as it turns out the implementation mixes up
    the order and having all weird side effects.
    
    For now it's better to deactivate the auto fix
    until the implementation (mostly happening)
    in the parser is fixed
    
    Closes #741
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- tests/test_class_oelint_newline_consecutive.py
+++ tests/test_class_oelint_newline_consecutive.py
@@ -24,23 +24,6 @@ class TestClassOelintNewlineConsecutive(TestBaseClass):
         self.check_for_id(self._create_args(input_), id_, occurrence)
 
     @pytest.mark.parametrize('id_', ['oelint.newline.consecutive'])
-    @pytest.mark.parametrize('input_',
-                             [
-                                 {
-                                     'oelint_adv_test.bb':
-                                     '''
-                                     A = "1"
-
-
-                                     B = "1"
-                                     ''',
-                                 },
-                             ],
-                             )
-    def test_fix(self, input_, id_):
-        self.fix_and_check(self._create_args_fix(input_), id_)
-
-    @pytest.mark.parametrize('id_', ['oelint.newline.consecutive'])
     @pytest.mark.parametrize('occurrence', [0])
     @pytest.mark.parametrize('input_',
                              [
