096387107e7ba0b39a42e8bc28b4eac5c92fed56
Konrad Weihmann <kweihmann@outlook.com>
2025-09-30 07:17:39 +0000

    repo: move to pyproject.toml
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-python3 setup.py build
-python3 setup.py sdist bdist_wheel --universal
-flake8
--- release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-./build.sh
-pre-commit run --all-files
-[ $? -ne 0 ] && echo "Pre-commit hooks failed" && exit 1
-pytest
-[ $? -ne 0 ] && echo "Tests failed" && exit 1
-twine upload dist/*
