45d297cfde98d0adf87efd50b4e8be7d137eb99b
Michal Sieron <michalwsieron@gmail.com>
2026-02-21 16:04:08 +0100

    oelint.vars.pathhardcode: Fix computational complexity
    There is no need to do have two nested loops just to check if any of the
    (pre, ext) combinations are in the line. One can easily search for a
    regex pattern which uses regex alternations.
    
    Fixes #842.
    
    Signed-off-by: Michal Sieron <michalwsieron@gmail.com>

--- oelint_adv/rule_base/rule_vars_pathhardcode.py
+++ oelint_adv/rule_base/rule_vars_pathhardcode.py
@@ -54,18 +54,18 @@ class VarsPathHardcode(Rule):
                 continue
             _matches = []
             for k, v in self._map.items():
-                for pre in ['^', "'", '"', ' ', r'\$\{D\}', '=']:
-                    for ext in [' ', '/', '"', r'\*', '$']:
-                        for line in i.get_items():
-                            if line.strip().startswith('#'):
-                                continue
-                            _match = RegexRpl.search(pre + k + ext, line)
-                            if _match and k not in _matches:
-                                _cleanapp = v.strip('$').strip('{').strip('}')
-                                res += self.finding(i.Origin, i.InFileLine,
-                                                    "'{a}' should be used instead of '{b}'".format(a=v, b=k), appendix=_cleanapp)
-                                _parts = [x for x in k.split('/') if x]
-                                for x in range(1, len(_parts) + 1):
-                                    _k = '/' + '/'.join(_parts[0:x])
-                                    _matches.append(_k)
+                pre = '(' + '|'.join(['^', "'", '"', ' ', r'\$\{D\}', '=']) + ')'
+                ext = '(' + '|'.join([' ', '/', '"', r'\*', '$']) + ')'
+                for line in i.get_items():
+                    if line.strip().startswith('#'):
+                        continue
+                    _match = RegexRpl.search(pre + k + ext, line)
+                    if _match and k not in _matches:
+                        _cleanapp = v.strip('$').strip('{').strip('}')
+                        res += self.finding(i.Origin, i.InFileLine,
+                                            "'{a}' should be used instead of '{b}'".format(a=v, b=k), appendix=_cleanapp)
+                        _parts = [x for x in k.split('/') if x]
+                        for x in range(1, len(_parts) + 1):
+                            _k = '/' + '/'.join(_parts[0:x])
+                            _matches.append(_k)
         return res
