cd55408a07eafca8a82a5dfc15c6e0a9b1f2c3bb
Michal Sieron <michalwsieron@gmail.com>
2026-02-22 20:04:34 +0100

    tests/downloadfilename: Fix typo in rule ID
    test_good had a typo in parametrized rule ID.
    Should be `vars` not `var`.
    For the same reason I renamed the file.
    
    Also fixed recipe names in the testfiles for test_good as the ID
    typo was actually hiding bad GuessRecipeVersion implementation in
    oelint-parser.
    
    For consistency I also renamed the recipe names in test_bad and added
    a new test case for another instance catching bad GuessRecipeVersion
    implementation.
    
    Signed-off-by: Michal Sieron <michalwsieron@gmail.com>

--- tests/test_class_oelint_vars_downloadfilename.py
+++ tests/test_class_oelint_vars_downloadfilename.py
@@ -10,52 +10,56 @@ class TestClassOelintVarDownloadfilename(TestBaseClass):
     @pytest.mark.parametrize('input_',
                              [
                                  {
-                                     'oelint_adv_test.bb':
+                                     'oelint-adv-test.bb':
                                      'SRC_URI += "http://foo.bar/baz.jpg"',
                                  },
                                  {
-                                     'oelint_adv_test.bb':
+                                     'oelint-adv-test.bb':
                                      'SRC_URI += "https://foo.bar/baz.jpg"',
                                  },
                                  {
-                                     'oelint_adv_test.bb':
+                                     'oelint-adv-test.bb':
                                      'SRC_URI += "ftp://foo.bar/baz.jpg"',
                                  },
                                  {
-                                     'oelint_adv_test.bb':
+                                     'oelint-adv-test.bb':
                                      'SRC_URI += "http://foo.bar/baz.jpg;downloadfilename=something.dat"',
+                                 },
+                                 {
+                                     'oelint-adv-test.bb':
+                                     'SRC_URI += "http://foo.bar/baz.jpg;downloadfilename=${PN}"',
                                  },
                              ],
                              )
     def test_bad(self, input_, id_, occurrence):
         self.check_for_id(self._create_args(input_), id_, occurrence)
 
-    @pytest.mark.parametrize('id_', ['oelint.var.downloadfilename'])
+    @pytest.mark.parametrize('id_', ['oelint.vars.downloadfilename'])
     @pytest.mark.parametrize('occurrence', [0])
     @pytest.mark.parametrize('input_',
                              [
                                  {
-                                     'oelint_adv_test.bb':
+                                     'oelint-adv-test.bb':
                                      'SRC_URI += "http://foo.bar/baz.jpg;downloadfilename=something.dat.${PV}"',
                                  },
                                  {
-                                     'oelint_adv_test.bb':
+                                     'oelint-adv-test.bb':
                                      'SRC_URI += "http://foo.bar/baz1.0.jpg"',
                                  },
                                  {
-                                     'oelint_adv_test_1.2.3.bb':
+                                     'oelint-adv-test_1.2.3.bb':
                                      'SRC_URI += "http://foo.bar/baz1.2.3.jpg"',
                                  },
                                  {
-                                     'oelint_adv_test.bb':
+                                     'oelint-adv-test.bb':
                                      'SRC_URI += "git://foo.bar/baz.git"',
                                  },
                                  {
-                                     'oelint_adv_test.bb':
+                                     'oelint-adv-test.bb':
                                      'SRC_URI += "http://foo.bar/baz.jpg;downloadfilename=something.${PV}"',
                                  },
                                  {
-                                     'oelint_adv_test.bb':
+                                     'oelint-adv-test.bb':
                                      '''
                                      PV = "1.2.3"
                                      SRC_URI += "http://foo.bar/baz.jpg;downloadfilename=something.1.2.3"
