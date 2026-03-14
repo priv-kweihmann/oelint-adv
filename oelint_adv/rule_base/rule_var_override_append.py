ca72f405e339556342c854dde541ad7f2d0dd582
Michal Sieron <michalwsieron@gmail.com>
2026-03-14 01:32:41 +0100

    oelint.vars.overrideappend: Extend check
    Using `A:class-target += " b"` actually appends to `A:class-target`,
    which will later override `A`. Most likely what was originally intended
    is conditional append to `A`.
    
    This commit extends oelint.vars.overrideappend rule to also check
    scenarios described above. Additionally I added a fixer for both old and
    new scenarios.
    
    Signed-off-by: Michal Sieron <michalwsieron@gmail.com>

--- oelint_adv/rule_base/rule_var_override_append.py
+++ oelint_adv/rule_base/rule_var_override_append.py
@@ -48,8 +48,44 @@ class VarOverrideAppend(Rule):
             'USERADD_PARAM',
         ]
 
-    def check(self, _file: str, stash: Stash) -> List[Tuple[str, int, str]]:
+    def fix_override_append_order(self, item, op, scope):
+        def replacer(v):
+            return v.replace(
+                item.VarNameComplete, item.VarName + ':' + op + ':' + scope,
+            )
+
+        item.Raw = replacer(item.Raw)
+        item.RealRaw = replacer(item.RealRaw)
+
+    def fix_pluseq_to_override(self, item, op_to_use, space):
+        varname = item.VarName
+        if item.VarName in self.pkgspecific:
+            varname += ':' + item.SubItems[0]
+
+        def replacer(v):
+            ret = v.replace(
+                varname, varname + ':' + op_to_use,
+            ).replace(
+                item.VarOp, ' = ',
+            )
+            if op_to_use == 'append':
+                opening_quote = ret.find(item.VarValue[0])
+                if space and ret[opening_quote + 1] not in ' \t\n':
+                    ret = ret[:opening_quote + 1] + space + ret[opening_quote + 1:]
+            else:  # must be `else`, or coverage is unhappy about always true
+                assert op_to_use == "prepend"
+                closing_quote = ret.rfind(item.VarValue[-1])
+                if space and ret[closing_quote - 1] not in ' \t\n':
+                    ret = ret[:closing_quote] + space + ret[closing_quote:]
+
+            return ret
+
+        item.Raw = replacer(item.Raw)
+        item.RealRaw = replacer(item.RealRaw)
+
+    def __getMatches(self, _file: str, stash: Stash):
         res = []
+
         items: List[Variable] = stash.GetItemsFor(filename=_file, classifier=Variable.CLASSIFIER,
                                                   attribute=Variable.ATTR_VAR)
 
@@ -59,14 +95,57 @@ class VarOverrideAppend(Rule):
             _items = [x for x in i.SubItems if x]
             if i.VarName in self.pkgspecific:
                 _items = _items[1:]
+
             _scope = [x for x in _items if x not in ['append', 'prepend']]
-            _op = [x for x in _items if x in ['append', 'prepend']]
-            if not _scope or not _items:
-                continue  # pragma: no cover
+            if not _scope:
+                # Skip if there is no override
+                continue
+
             _scope_key = i.VarName + ':' + '-'.join(_scope)
-            if _op:
-                if _items[0] not in _op and _scope_key not in _store:
-                    res += self.finding(i.Origin, i.InFileLine,
-                                        override_msg=self.Msg.format(op=_op[0], _class=':'.join(_scope)))
+            if _scope_key in _store:
+                # Skip if the override scope exists. This operation may be intentional
+                continue
+
+            extra_check = []
+            extra_fix = []
+            _op = [x for x in _items if x in ['append', 'prepend']]
+            if i.VarOp.strip() in ('+=', '.=', '=+', '=.') and not _op:
+                # Case: A:foo += "b" -> A:append:foo = " b"
+                op_to_use = 'prepend' if i.VarOp.strip()[0] == '=' else 'append'
+                space = ' ' if '+' in i.VarOp else ''
+
+                extra_check = [op_to_use, ':'.join(_scope)]
+                extra_fix = [self.fix_pluseq_to_override, op_to_use, space]
+            elif _op and _items[0] not in _op:
+                # Case: A:foo:append = " b" -> A:append:foo = " b"
+                extra_check = [_op[0], ':'.join(_scope)]
+                extra_fix = [self.fix_override_append_order, _op[0], ':'.join(_scope)]
+
             _store.add(_scope_key)
+            if extra_check or extra_fix:
+                res.append((i, extra_check, extra_fix))
+
+        return res
+
+    def check(self, _file: str, stash: Stash) -> List[Tuple[str, int, str]]:
+        res = []
+        for i, extra_check, _extra_fix in self.__getMatches(_file, stash):
+            res += self.finding(
+                i.Origin,
+                i.InFileLine,
+                override_msg=self.Msg.format(
+                    op=extra_check[0],
+                    _class=extra_check[1],
+                ),
+            )
+
+        return res
+
+    def fix(self, _file: str, stash: Stash) -> List[str]:
+        res = []
+        for i, _extra_check, extra_fix in self.__getMatches(_file, stash):
+            fixer, *args = extra_fix
+            fixer(i, *args)
+            res.append(_file)
+
         return res
--- tests/test_class_oelint_vars_overrideappend.py
+++ tests/test_class_oelint_vars_overrideappend.py
@@ -27,6 +27,22 @@ class TestClassOelintVarsOverrideAppend(TestBaseClass):
                                      A:qemux86-64:prepend = "a"
                                      ''',
                                  },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'A:class-target += "a"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'ALTERNATIVE:${PN}:foo =+ "a"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'ALTERNATIVE:${PN}-test:foo =+ "a"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'A:class-target =. "a"',
+                                 },
                              ],
                              )
     def test_bad(self, input_, id_, occurrence):
@@ -63,7 +79,65 @@ class TestClassOelintVarsOverrideAppend(TestBaseClass):
                                      FILES:${PN} = "a"
                                      ''',
                                  },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'ALTERNATIVE:${PN} += "a"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     A:class-target = ""
+                                     A:class-target =+ "a"
+                                     '''
+                                 },
                              ],
                              )
     def test_good(self, input_, id_, occurrence):
         self.check_for_id(self._create_args(input_), id_, occurrence)
+
+    @pytest.mark.parametrize('id_', ['oelint.vars.overrideappend'])
+    @pytest.mark.parametrize('occurrence', [1])
+    @pytest.mark.parametrize('input_',
+                             [
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     A:class-target:append = " a"
+                                     ''',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     A:class-target:prepend = "a"
+                                     ''',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     A:qemux86-64:prepend = "a"
+                                     ''',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'A:class-target += " a"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'A:class-target .= "a"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'ALTERNATIVE:${PN}:foo =+ "a"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'ALTERNATIVE:${PN}-test:foo =+ "a"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'A:class-target =. "a"',
+                                 },
+                             ],
+                             )
+    def test_fix(self, input_, id_, occurrence):
+        self.fix_and_check(self._create_args_fix(input_), id_)
