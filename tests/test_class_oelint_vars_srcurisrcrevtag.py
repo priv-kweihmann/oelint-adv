25d9afda29ebfb728525524e7c777e52f761b678
Konrad Weihmann <kweihmann@outlook.com>
2026-03-19 08:55:34 +0000

    oelint.vars.srcurisrcrevtag: disable post whinlatter
    as upstream changed the behavior
    
    Closes #864
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- tests/test_class_oelint_vars_srcurisrcrevtag.py
+++ tests/test_class_oelint_vars_srcurisrcrevtag.py
@@ -6,7 +6,7 @@ from .base import TestBaseClass
 class TestClassOelintVarsSRCURIRevTag(TestBaseClass):
 
     @pytest.mark.parametrize('id_', ['oelint.vars.srcurisrcrevtag'])
-    @pytest.mark.parametrize('occurrence', [1])
+    @pytest.mark.parametrize('occurrence', [0])
     @pytest.mark.parametrize('input_',
                              [
                                  {
@@ -27,6 +27,30 @@ class TestClassOelintVarsSRCURIRevTag(TestBaseClass):
                              )
     def test_bad(self, input_, id_, occurrence):
         self.check_for_id(self._create_args(input_), id_, occurrence)
+
+    @pytest.mark.parametrize('id_', ['oelint.vars.srcurisrcrevtag'])
+    @pytest.mark.parametrize('occurrence', [1])
+    @pytest.mark.parametrize('input_',
+                             [
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     SRC_URI += "git://foo.org/gaz.git;tag=${PV};name=foo"
+                                     SRCREV_foo = "1234"
+                                     ''',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     SRC_URI += "git://foo.org/gaz.git;tag=${PV}"
+                                     SRCREV = "1234"
+                                     ''',
+                                 },
+                             ],
+                             )
+    def test_bad_pre_whinlatter(self, input_, id_, occurrence):
+        self.check_for_id(self._create_args(
+            input_, ['--release=scarthgap']), id_, occurrence)
 
     @pytest.mark.parametrize('id_', ['oelint.vars.srcurisrcrevtag'])
     @pytest.mark.parametrize('occurrence', [0])
@@ -69,3 +93,46 @@ class TestClassOelintVarsSRCURIRevTag(TestBaseClass):
                              )
     def test_good(self, input_, id_, occurrence):
         self.check_for_id(self._create_args(input_), id_, occurrence)
+
+    @pytest.mark.parametrize('id_', ['oelint.vars.srcurisrcrevtag'])
+    @pytest.mark.parametrize('occurrence', [0])
+    @pytest.mark.parametrize('input_',
+                             [
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                        SRC_URI += "git://foo.org/gaz.git;name=bar"
+                                        SRCREV_bar = "1234"
+                                        ''',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                        SRC_URI += "git://foo.org/gaz.git"
+                                        SRCREV = "1234"
+                                        ''',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                        SRC_URI += "git://foo.org/gaz.git;tag=${PV};name=foo"
+                                        SRCREV_bar = "1234"
+                                        ''',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'SRC_URI[md5sum] = "file://abc"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'SRC_URI[sha256sum] = "file://abc"',
+                                 },
+                                 {
+                                     'oelint_adv_test.bb':
+                                     'SRC_URI = "${@magic.foo.operation}"',
+                                 },
+                             ],
+                             )
+    def test_good_pre_whinlatter(self, input_, id_, occurrence):
+        self.check_for_id(self._create_args(
+            input_, ['--release=scarthgap']), id_, occurrence)
