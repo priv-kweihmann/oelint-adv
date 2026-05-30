c10c0adb129037d6755de4a1a376b6388314f70d
Konrad Weihmann <kweihmann@outlook.com>
2026-05-30 10:08:52 +0200

    oelint.vars.layerconf: fix for old syntax
    as the _ splits the var name into 2 parts
    when running with the old syntax,
    we need to look at VarNameCompleteNoModifiers
    instead.
    This removes false positives on e.g. dunfell
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- tests/test_class_oelint_vars_layerconf.py
+++ tests/test_class_oelint_vars_layerconf.py
@@ -50,6 +50,30 @@ class TestClassOelintVarsLayerConf(TestBaseClass):
         'LICENSE_PATH',
     ])
     @pytest.mark.parametrize('operation', ['=', ':=', '.=', '=.', '+=', '=+', ' =+'])
+    def test_good_old_syntax(self, id_, var, operation, occurrence):
+        input_ = {
+            'conf/layer.conf': self.__generate_sample_code(var, operation),
+        }
+        self.check_for_id(self._create_args(
+            input_, ['--release=dunfell']), id_, occurrence)
+
+    @pytest.mark.parametrize('id_', ['oelint.vars.layerconf'])
+    @pytest.mark.parametrize('occurrence', [0])
+    @pytest.mark.parametrize('var', [
+        'BBFILES',
+        'BBFILES_DYNAMIC',
+        'BBFILE_COLLECTIONS',
+        'BBFILE_PATTERN_foo',
+        'BBFILE_PRIORITY_foo',
+        'BBPATH',
+        'HOSTTOOLS_NONFATAL',
+        'LAYERDEPENDS_foo',
+        'LAYERRECOMMENDS_foo',
+        'LAYERSERIES_COMPAT_foo',
+        'LAYERVERSION_foo',
+        'LICENSE_PATH',
+    ])
+    @pytest.mark.parametrize('operation', ['=', ':=', '.=', '=.', '+=', '=+', ' =+'])
     def test_good_non_layer(self, id_, var, operation, occurrence):
         input_ = {
             'oelint-test_1.0.bb': self.__generate_sample_code(var, operation),
